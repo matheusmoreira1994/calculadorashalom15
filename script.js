07c188358c02a5f93ba3d3cf0e7146ff8462fbb0
matheusmoreira1994 <114697713+matheusmoreira1994@users.noreply.github.com>
2023-10-26 23:54:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function calcular() {
+    // Pega o valor digitado pelo usuário
+    const valor = document.getElementById("valor").value;
+
+    // Calcula 10% e 5% do valor
+    const dezPorcento = (valor * 0.1).toFixed(2);
+    const cincoPorcento = (valor * 0.05).toFixed(2);
+
+    // Soma os valores
+    const soma = (parseFloat(dezPorcento) + parseFloat(cincoPorcento)).toFixed(2);
+
+    // Exibe os resultados na página
+    document.getElementById("dezPorcento").textContent = dezPorcento;
+    document.getElementById("cincoPorcento").textContent = cincoPorcento;
+    document.getElementById("soma").textContent = soma;
+}
+ function irparasite() {
+        window.open = "https://www.google.com/search?q=site%3Awww.terra.com.br";
+ }
